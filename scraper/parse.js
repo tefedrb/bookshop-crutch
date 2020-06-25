b4bbf11c0b240a996f0a75836d4302697cac85fe
tefedrb-enterprise <tefebell@gmail.com>
2020-06-25 09:00:32 -0400

    updated parse.js - now finds both usps and ups tracking - modularized the process - importing into scraper.js

--- scraper/parse.js
+++ scraper/parse.js
@@ -1,6 +1,7 @@
 const pdf = require('pdf-parse');
 const fs = require('fs');
 
+// Try: instead of parsing the pdf as a string, turn into .JSON and grab properties
 
 function writeToTxtFile(data, fileName){
     pdf(data).then(res => {
@@ -11,46 +12,56 @@ function writeToTxtFile(data, fileName){
     });
 }
 
-const testing = (poNumber) => {
+const getTrackingNumber = async (poInput) => {
+    // Maybe use readFile instead of sync... but it seems like we need to use the buffer
+    // in order for pdf() to read it?
     let dataBuffer = fs.readFileSync('ibg.customer.creditstatus.pdf');
-
-    pdf(dataBuffer).then(function(res){
+    let output;
+    await pdf(dataBuffer).then(function(res){
         // Find the PO - capture index number
         const testText = res.text;
-        // poNumber will go here instead - lets also develop a regex for po numbers
-        const poNum = /R\d{9}/;
 
-        const regex = /R659880032/;
-        const capturedMatch = testText.match(regex);
-        const poIndex = capturedMatch["index"];
+        const capturedMatch = testText.match(poInput);
+        
+        // lets also develop a regex for po numbers
+        const poRegex = /R\d{9}/;
+        console.log(capturedMatch[0], "captured match");
+        if(capturedMatch){
+            const poIndex = capturedMatch["index"];
 
-        // Slice this to a smaller size...
-        const firstSlice = testText.slice((poIndex + 10));
+            // Slice this to a smaller size...
+            const firstSlice = testText.slice((poIndex + 10));
 
-        // We want to Slice again to find the next order (Allows us to search only between our order 
-        // and the next, narrowing down on potential edge cases)
-        const nextOrderInSequence = firstSlice.match(poNum);
+             // We want to Slice again to find the next order (Allows us to search only between our order 
+            // and the next, narrowing down on potential edge cases)
+            const secondSlice = firstSlice.match(poRegex);
 
-        const sliced = firstSlice.slice(0, nextOrderInSequence["index"]);
-        console.log(sliced, "NEW TEXT");
+            const sliced = firstSlice.slice(0, secondSlice["index"]);
 
-        // Develop regex to search for USPS tracking -
-        const trackingRegex = /\d{22}/;
+            /** 
+                regex formats
+            **/
+            const trackingFormat = {
+                ups: /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/,
+                usps: /\d{22}/
+            };
 
-        // Develop regex to search for UPS tracking -
-        const upsTrackingReg = /\b(1Z ?[0-9A-Z]{3} ?[0-9A-Z]{3} ?[0-9A-Z]{2} ?[0-9A-Z]{4} ?[0-9A-Z]{3} ?[0-9A-Z]|[\dT]\d\d\d ?\d\d\d\d ?\d\d\d)\b/;
+            const uspsTracking = sliced.match(trackingFormat.usps);
 
-        // Search sliced text to find first instance of tracking regex
-        const searchSliced = sliced.match(trackingRegex);
-        const trackingIndex = searchSliced["index"];
-
-        // return a substring! from beginning to 23 places in of tracking
-        console.log(sliced.substring(trackingIndex, trackingIndex + 23), "SHOULD BE tracking");
+            if(uspsTracking){
+                output = uspsTracking[0];
+            } else {
+                output = sliced.match(trackingFormat.ups)[0];
+            }
+        } else {
+            output = false;
+        }
     })
     .catch(err => {
         console.log("PDF-Parse err: ", err);
     })
-    
+    console.log(output);
+    return output;
 };
 
-testing();
+exports.getTrackingNumber = getTrackingNumber;
