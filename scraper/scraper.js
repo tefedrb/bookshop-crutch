d66d7187d4629d7815b4f95d7db52ba287b175b9
tefedrb-enterprise <tefebell@gmail.com>
2020-06-25 22:15:47 -0400

    updated logic to accept orders with backorders

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -2,13 +2,14 @@
 // Then save information about each books status, quantity, cost etc.
 // Consider breaking these steps up into files and exporting / importing them as modules
 // Consider having an open page running so you only need to loging to Ingram ONCE.
+// Have to watch out for expired sessions.
 
 // IMPORT PARSE.GETTRACKINGNUMBER(poNum) into scraper...
 
 const dotenv = require('dotenv');
 dotenv.config();
 const puppeteer = require('puppeteer');
-const tracking = require('parse'); 
+const tracking = require('./parse'); 
 
 // const pdf = require('pdf-parse');
 const crawler = require('crawler-request');
@@ -69,37 +70,52 @@ const loginToIngram = async (data, crawler) => {
         // Need to wait for page to settle
         await page.waitForNavigation();
 
+        /**** COLLECT ORDER INFO ****/
         await page.evaluate(() => {
             const orders = [];
-            const filterOrders = (orderRows) => {
+            const filterOrders = (orderRows, hasBackOrders) => {
+                console.log("did this")
                 return orderRows.filter((row, idx) => {
-                    if(row.children[2].firstChild.text && idx != 0){
+                    console.log(row.children);
+                    console.log(row.children[hasBackOrders ? 3 : 2].firstChild.text, "firstChild");
+                    if(row.children[hasBackOrders ? 3 : 2].firstChild.text && idx != 0){
                         return true;
                     }
                 })
             }
             /**  Check to see if column 3 (2) has an EAN. If not, it's not an order, its probably 
              shipping etc. **/
+
+            /*** UPDATE 6/25/20 need to account for backorder/preorder column ***/
+            // Manually creating labels...for orderRows.forEach loop
+            const labels = [
+                "Date Ordered", "Status", "Ean", "Product Name",
+                "Format", "Pub Date", "Po Number", "OE Number", 
+                "Qty", "price", "Invoice Number", "DC"
+            ]
+
              const orderTableContainer = document.querySelector("form[name=backorderedForm]");
-             const orderTable = orderTableContainer.firstElementChild.children[3];
-             const allRows = Array.from(orderTable.firstElementChild.children);
+             const orderTable = orderTableContainer.firstElementChild.children;
+
+            // If .innerBoundryBox contains 6 children elements - it has backorders (checkboxes)
+             const hasBackOrders = orderTable.length === 6 ? true : false;
+             let allRows = Array.from(orderTable[hasBackOrders ? 4 : 3].firstElementChild.children);
+        
+            if(hasBackOrders) {
+                allRows = Array.from(orderTable[4].firstElementChild.children)
+                labels.unshift("Select");
+            }
 
             //  Filter out rows with no EAN
-            const orderRows = filterOrders(allRows);
+            const orderRows = filterOrders(allRows, hasBackOrders);
            
             // Insert date ordered - EAN - Product Name - Format - PO Number - QTY - Invoice# (w link) - DC
             orderRows.forEach(row => {
                 const saveOrder = {};
-                // Manually creating labels...
-                const labels = [
-                    "Date Ordered", "Status", "Ean", "Product Name",
-                    "Format", "Pub Date", "Po Number", "OE Number", 
-                    "Qty", "price", "Invoice Number", "DC"
-                ]
-                // Target each COLUMN and save to orders
+                // Target each COLUMN and save to orders -
                 for(let i = 0; i < row.children.length; i++){
                     const entry = row.children[i];
-                    const entryHasHref = entry.firstChild.href || (entry.firstElementChild && entry.firstElementChild);
+                    const entryHasHref = entry.firstChild.href || (entry.firstElementChild && entry.firstElementChild.href);
                     if(entryHasHref){
                         saveOrder[labels[i]] = [row.children[i].innerText, row.children[i].firstChild.href || entry.firstElementChild.href]
                     } else {
@@ -125,7 +141,34 @@ const loginToIngram = async (data, crawler) => {
         // Consider that there may be different invoices on one order
         const pdfPage = await browser.newPage();
 
-        // Need to specify to continue ONLY IF "Invoice Number" isn't null
+        console.log(orderData, "AGAIN NOW.")
+
+        // const getAllInvoices = [];
+        // orderData.reduce((acc, order) => {
+        //     if(acc[order[]])
+        //     acc[]
+        // }, {})
+        
+
+        // Holding invoices and associated 'orders' - will begin the search for tracking numbers
+        const holdInvoices = {};
+        orderData.forEach((order, idx) => {
+            const invoice = order["Invoice Number"];
+            const invoiceLink = typeof invoice === "object" ? invoice[1] : false;
+            
+            if(invoiceLink){
+                // ex: {'46322755': [https://ipage...]}
+                if(!holdInvoices[invoice[0]]){
+                    // if invoice hasn't been stored in holdInovices
+                    holdInvoices[invoice[0]] = [invoice[1], idx];
+                } else {
+                    // add the idx numbers of each order that shares the same invoice
+                    holdInvoices[invoice[0]].push(idx);
+                }
+            }
+        })
+
+        // The question is to download invoices or try to parse them as is - on the page
         await pdfPage.goto(orderData[0]["Invoice Number"][1]);
 
         // await pdfPage.addScriptTag({ path: '../node_modules/crawler-request/crawler-request.js' })
@@ -133,22 +176,7 @@ const loginToIngram = async (data, crawler) => {
 
         /** instead of evaluating page using puppeteer, we will just use crawler-request within node
           and hopefully capture data in this namespace to do whatever with */
-        let dataBuffer = await fs.readFile('ibg.customer.credistatus.pdf');
 
-        await pdf(databuffer).then(res => {
-            console.log(res.numpages);
-        })
-        
-        await pdfPage.evaluate(() => {
-            console.log("we made it!");
-            // PDF Parse time
-            console.log(this.location.href, "THIS");
-            console.log(window, "WINDOW");
-            // window["crawler-request"].crawler_request_wrapper(this.location.href).then(res => {
-            //     console.log(res.text.length);
-            // })
-            
-        })
     }catch(err){
         console.log("Error after scraping order page: ",  err.message)
     }
