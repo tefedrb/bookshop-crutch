d47ce0ec5c5d40d3e4c70a0140eaf285f03aac0f
tefedrb-enterprise <tefebell@gmail.com>
2020-07-18 09:32:55 -0400

    threaded book info into backorders - refactored modal.js to destructure/reformat bookData instead of book doing it

--- client/src/components/Backorder.js
+++ client/src/components/Backorder.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Modal from './Modal';
 
 function Backorder(props) {
-    let [moreInfo, setMoreInfo] = useState(false)
-
-    const toggleInfo = () => setMoreInfo(!moreInfo)
+    const [moreInfo, setMoreInfo] = useState(false);
+    const [selectedModal, setSelectedModalInfo] = useState("");
+    
+    const toggleInfo = (event) => {
+        console.log(event, "event with a click")
+        setMoreInfo(!moreInfo);
+        if(moreInfo){
+            setSelectedModalInfo()
+        }
+    }
 
     const { unshipped } = props;
     // This will be taking in books not on any shipment
     // [{}, {}]
-
+    console.log(unshipped[0], "UNSHIPPED")
     // const { 
     //     DC, 'Date Ordered': dateOrdered, 
     //     Ean: ean, 
@@ -34,22 +41,31 @@ function Backorder(props) {
         // pubDate: "June 16, 2020"
         // NEED TO GET ARRIVAL DATE
     // }
+    // useEffect(() => {
+    //     console.log(unshipped[0].modalInfo, "MODAL INFO");
+    //     if(unshipped[0].modalInfo){
+    //         setModalInfo(true);
+    //     }
+    // }, [unshipped])
 
     const rows = unshipped?.map((book, idx) => {
         const { 'Product Name': productName, 'Pub Date': pubDate} = book;
+        console.log(book.modalInfo, "MODAL INFO SHOWING UP?!")
         const { onHand, onOrder } = book.modalInfo || {};
+        console.log(onHand, "ON HAND?!")
+        console.log(onHand != undefined ? "yes" : "no", "WITHIN THIS THANG");
+        console.log(typeof onHand, "WHAT IS IT?!");
         return (
             <tr key={idx}>
                 <td className='more-info' onClick={toggleInfo}>{productName}</td>
                 <td>{pubDate}</td>
-                <td>{onHand ? onHand : "Loading..."}</td>
-                <td>{onOrder ? onOrder : "Loading..."}</td>
+                <td>{onHand != undefined ? onHand : "Loading..."}</td>
+                <td>{onOrder != undefined ? onOrder : "Loading..."}</td>
                 <td>Loading...</td>
             </tr>
         )
     })
     // <h3>{props.title}{props.data.length > 1 ? `s (${props.data.length})` : ''}</h3>
-
     return (
         <div className='backorders'>
             <h3>{unshipped.length > 1 ? `Unshipped (${unshipped.length})` : ''}</h3>
@@ -65,7 +81,7 @@ function Backorder(props) {
                     {rows}
                 </tbody>
             </table>
-            {moreInfo ? <Modal data={'ye'} toggleInfo={toggleInfo} /> : ''}
+            {moreInfo ? <Modal data={unshipped[0]} toggleInfo={toggleInfo} /> : ''}
         </div>
     )
 }
